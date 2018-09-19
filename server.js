fc23870416ec844f0e35f5b10c03ca090e325b2b
Joshua Smith <veryjoshua@gmail.com>
2018-09-18 23:50:34 -0400

    finiish stuff

--- server.js
+++ server.js
@@ -3,18 +3,22 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 const axios =  require('axios');
 const app = express();
+const path = require('path');
 require('dotenv').config();
 const controller = require('./server/controller')
 const functions = require('./server/functions');
 
 app.use(cors());
 app.use(bodyParser.json());
+// app.use('/', express.static('index.html'));
 
 
 app.get('/api/wait_times/:park', controller.parkInfo)
 app.get('/api/best_park', controller.getBestPark)
-
-const port = process.env.PORT || 8000;
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+const port = process.env.PORT || 8070;
 
 app.listen(port, ()=>{
     functions.syncCache();
